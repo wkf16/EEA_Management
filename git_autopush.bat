04e496dcf27b5cf66870600db38276188661d1dd
he he <wkf16@example.com>
2023-10-26 23:18:35 +0800

    update 2023/10/26 周四

--- git_autopush.bat
+++ git_autopush.bat
@@ -1,8 +1,10 @@
 @echo off
-
-:: 获取当前日期
-for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
-set "datestamp=%datetime:~6,4%/%datetime:~4,2%/%datetime:~0,4%"
+for /F "tokens=1-3 delims=/" %%a in ('date /t') do (
+    set dd=%%a
+    set mm=%%b
+    set yyyy=%%c
+)
+set "datestamp=%dd%/%mm%/%yyyy%"
 
 :: 进行git操作
 git add .
